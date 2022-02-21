8e767afd5423951a99ff16e15efa2a13a6180923
YooSeokSo <koreanso@naver.com>
2022-02-21 15:20:40 +0900

    add ConnectFTP.kt (apache FTP)

--- app/src/main/java/com/example/lte_signal_test/MainActivity.kt
+++ app/src/main/java/com/example/lte_signal_test/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.lte_signal_test
 
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.pm.PackageManager
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
@@ -9,53 +10,94 @@ import android.os.Bundle
 import android.telephony.CellInfoLte
 import android.telephony.*
 import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.Main
+import kotlinx.coroutines.launch
+import org.apache.commons.net.ftp.FTPClient
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.concurrent.thread
 
 
 class MainActivity : AppCompatActivity() {
-
-    var rsrp = 0
-    var rsrq = 0
-    var snr = 0
-    var lteantlevel = 0
-    var pci = 0
-    var earfcn = 0
-    var TAG = "LTESIGNAL"
-
-    var permissions = arrayOf(
+    private var rsrp = 0
+    private var rsrq = 0
+    private var snr = 0
+    private var lteantlevel = 0
+    private var pci = 0
+    private var earfcn = 0
+    private var TAG = "LTESIGNAL"
+    val scope = CoroutineScope(Main)
+    private var permissions = arrayOf(
         Manifest.permission.ACCESS_FINE_LOCATION,
         Manifest.permission.READ_PHONE_STATE,
         Manifest.permission.INTERNET,
         Manifest.permission.ACCESS_COARSE_LOCATION,
     )
 
+    @SuppressLint("SetTextI18n", "SimpleDateFormat")
     @RequiresApi(Build.VERSION_CODES.R)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        checkPermissions()
 
-        val cellInfoList: List<CellSignalStrength>
+        val textBox = findViewById<TextView>(R.id.textView)
+        val saveButton = findViewById<Button>(R.id.button)
+
+        var cellInfoList: List<CellSignalStrength>
         val tm = getSystemService(TELEPHONY_SERVICE) as TelephonyManager
-        cellInfoList = tm.signalStrength!!.cellSignalStrengths
-        for (cellInfo in cellInfoList) {
-            if (cellInfo is CellSignalStrengthLte) {
-                rsrp = cellInfo.rsrp
-                rsrq = cellInfo.rsrq
-                snr = cellInfo.rssnr
-                lteantlevel = cellInfo.level
-                Log.e(TAG, "snr : " + cellInfo.toString())
+        val format = SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+        var time: String = format.format(Date())
+        thread {
+            while (true) {
+                time= format.format(Date())
+                cellInfoList = tm.signalStrength!!.cellSignalStrengths
+                for (cellInfo in cellInfoList) {
+                    if (cellInfo is CellSignalStrengthLte) {
+                        rsrp = cellInfo.rsrp
+                        rsrq = cellInfo.rsrq
+                        snr = cellInfo.rssnr
+                        lteantlevel = cellInfo.level
+                        Log.e(TAG, "snr : $cellInfo")
+                    }
+                }
+                val cellIdentityList: List<CellInfo> = tm.allCellInfo
+                for (cellinfo in cellIdentityList) {
+                    if (cellinfo is CellInfoLte) {
+                        pci = cellinfo.cellIdentity.pci
+                        earfcn = cellinfo.cellIdentity.earfcn
+                    }
+                }
+                scope.launch {
+                    textBox.text =
+                        "TIME : $time \nPCI : $pci \nEARFCN : $earfcn \nRSRP : $rsrp \nRSRQ : $rsrq \nSNR : $snr \nANR LEVEL : $lteantlevel"
+                }
+                Thread.sleep(5000L)
             }
         }
-        checkPermissions()
-        val cellIdentityList: List<CellInfo> = tm.allCellInfo
-        for (cellinfo in cellIdentityList){
-            if(cellinfo is CellInfoLte){
-                pci = cellinfo.cellIdentity.pci
-                earfcn = cellinfo.cellIdentity.earfcn
+        // 데이터 CSV 파일로 저장
+        saveButton.setOnClickListener {
+            val filePath = filesDir.toString()
+            val csvHelper = CsvHelper(filePath)
+            val dataList = arrayListOf<Array<String>>()
+            if (!File("$filePath/LTESignal").exists())
+            {
+                dataList.add(arrayOf("TIME", "PCI", "Earfcn", "RSRP", "RSRQ", "SNR", "Ant Level"))
             }
+            dataList.add(arrayOf(time,pci.toString(),earfcn.toString(),rsrp.toString(),rsrp.toString(),rsrq.toString(),snr.toString(),lteantlevel.toString()))
+
+            csvHelper.writeData("LTESignal", dataList)
+            Toast.makeText(this,"현재 로그 저장",Toast.LENGTH_SHORT).show()
         }
         Log.e(TAG, "pci : $pci")
         Log.e(TAG, "earfcn : $earfcn")
@@ -66,21 +108,12 @@ class MainActivity : AppCompatActivity() {
 
 
 
-        // 데이터 CSV 파일로 저장
-        val filePath = filesDir.toString()
 
-        val csvHelper = CsvHelper(filePath)
 
-        val dataList = arrayListOf<Array<String>>()
-
-        dataList.add(arrayOf("TIME","PCI","Earfcn","RSRP","RSRQ","SNR","Ant Level"))
-        //dataList.add(arrayOf("서유석","서유석"))
-
-        csvHelper.writeData("LTESignal", dataList)
     }
     private fun checkPermissions() {
         //거절되었거나 아직 수락하지 않은 권한(퍼미션)을 저장할 문자열 배열 리스트
-        var rejectedPermissionList = ArrayList<String>()
+        val rejectedPermissionList = ArrayList<String>()
 
         //필요한 퍼미션들을 하나씩 끄집어내서 현재 권한을 받았는지 체크
         for(permission in permissions){
--- app/src/main/java/com/example/lte_signal_test/PermissionSuppoert.kt
+++
@@ -1,6 +0,0 @@
-package com.example.lte_signal_test
-
-import android.content.Context
-import android.app.Activity
-import java.util.jar.Manifest
-
